87c599e5957c8be1974af5fe94fa529cc3d24d92
muriloandrei <124423711+muriloandrei@users.noreply.github.com>
2023-07-22 11:18:16 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,63 @@
+// 1. Crie um script que exiba a mensagem "Hello World!" em um alerta no navegador.
+    // console.log(alert("Hello World!"));
+
+// 2. Crie um script que declare duas variáveis e exiba o resultado da soma entre elas.
+    // let numberOne = Number(prompt("Insira um Número."));
+    // let numberTwo = Number(prompt("Insira mais um Número."));
+    // let soma = numberOne + numberTwo
+    //  alert(`A soma dos números é: ${soma}`);
+
+// 3. Crie um script que declare uma variável e verifique se o seu valor é um número. Se for, exiba a mensagem "É um número", caso contrário, exiba a mensagem "Não é um número".
+    // let variavel = 10;
+    // if (typeof variavel === "number") {
+    //     console.log("É um número");
+    // } else {
+    //     console.log("Não é um número");
+    // }
+//💡 Para saber o tipo de dado você pode usar o operador `typeof`
+    
+// 4. Crie um script que declare uma variável e verifique se o seu valor é uma string. Se for, exiba a mensagem "É uma string", caso contrário, exiba a mensagem "Não é uma string".
+    // let variavel = "O céu é azul";
+    // if (typeof variavel === "string") {
+    //     console.log("É uma string.");
+    // } else {
+    //     console.log("Não é uma string");
+    // }
+//5. Crie um script que declare uma variável e verifique se o seu valor é um booleano. Se for, exiba a mensagem "É um booleano", caso contrário, exiba a mensagem "Não é um booleano".
+    // let variavel = true;
+    // if (typeof variavel === "boolean") {
+    //     console.log("É um booleano.");
+    // } else {
+    //     console.log("Não é um booleano.");
+    // }
+// 6. Crie um script que declare duas variáveis e exiba o resultado da subtração entre elas.
+    // let numberOne = Number(prompt("Insira um Número."));
+    // let numberTwo = Number(prompt("Insira mais um Número."));
+    // let subtraçao = numberOne - numberTwo
+    //  alert(`A subtração dos números é: ${subtraçao}`);
+// 7. Crie um script que declare duas variáveis e exiba o resultado da multiplicação entre elas.
+    // let numberOne = Number(prompt("Insira um Número."));
+    // let numberTwo = Number(prompt("Insira mais um Número."));
+    // let multiplicaçao = numberOne * numberTwo
+    //  alert(`A multiplicação dos números é: ${multiplicaçao}`);
+
+// 8. Crie um script que declare duas variáveis e exiba o resultado da divisão entre elas.
+    // let numberOne = Number(prompt("Insira um Número."));
+    // let numberTwo = Number(prompt("Insira mais um Número."));
+    // let divisao = numberOne / numberTwo
+    //  alert(`A divisão dos números é: ${divisao}`);
+
+// 9. Crie um script que declare uma variável e verifique se o seu valor é um número par. Se for, exiba a mensagem "É um número par", caso contrário, exiba a mensagem "Não é um número par".
+    // let variavel = prompt("Insira um Número.");
+    // if (variavel % 2 === 0) {
+    //     alert("É um número par.");
+    // } else { 
+    //     alert("Não é um número par.");
+    // }
+// 10. Crie um script que declare uma variável e verifique se o seu valor é um número ímpar. Se for, exiba a mensagem "É um número ímpar", caso contrário, exiba a mensagem "Não é um número ímpar".
+    // let variavel = prompt("Insira um Número.");
+    // if (variavel % 2 === 1) {
+    //     alert("É um número ímpar.");
+    // } else {
+    //     alert("Não é um número ímpar.");
+    // }
